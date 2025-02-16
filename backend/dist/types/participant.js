4091445f8205945c759a426251509d1131d05312
ShreeshRD <shreeshrdevi2@gmail.com>
2025-02-16 19:05:20 +0530

    removed dist folder

--- backend/dist/types/participant.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ParticipantSchema = void 0;
-const zod_1 = require("zod");
-exports.ParticipantSchema = zod_1.z.object({
-    Employee_Name: zod_1.z.string().min(1),
-    Employee_EmailID: zod_1.z.string().email(),
-});
